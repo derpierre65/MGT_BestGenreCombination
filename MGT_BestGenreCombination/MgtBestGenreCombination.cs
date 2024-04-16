01b59a99adf4dc7af0867552f735cbbe880add12
derpierre65 <git@derpierre65.de>
2024-04-16 22:42:14 +0100

    chore: initial commit

---
+++ MGT_BestGenreCombination/MgtBestGenreCombination.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MGT_BestGenreCombination
+{
+    public class MgtBestGenreCombination : MelonMod
+    {
+    }
+
+    [HarmonyPatch(typeof(Menu_DevGame_Genre), "Init")]
+    public static class PatchMenuGUIMainStart
+    {
+        static void Postfix(Menu_DevGame_Genre __instance, ref GUI_Main ___guiMain_, ref genres ___genres_, ref Menu_DevGame ___mDevGame_)
+        {
+            // __instance.genreArt 0 = main genre
+            // __instance.genreArt 1 = sub genre
+            var firstGenreId = __instance.genreArt == 1 ? ___mDevGame_.g_GameMainGenre : ___mDevGame_.g_GameSubGenre;
+            var genreButtons = __instance.uiObjects[0].transform.GetComponentsInChildren<Item_DevGame_Genre>();
+            foreach (var genreButton in genreButtons)
+            {
+                var color = Color.white;
+                if (firstGenreId != -1 && ___genres_.genres_COMBINATION[firstGenreId, genreButton.myID])
+                {
+                    color = ___guiMain_.colors[4];
+                }
+
+                genreButton.GetComponent<Image>().color = color;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+﻿Adds the function that shows which genre matches to the genre you have selected.
+
+![img.png](images/img.png)
+
+## Installation
+1. Download the [latest release](https://github.com/LavaGang/MelonLoader/releases) of MelonLoader and install it for Mad Games Tycoon 2.
+2. Download the [latest release](https://github.com/derpierre65/MGT_BestGenreCombination/releases) release and extract to the mods directory.
